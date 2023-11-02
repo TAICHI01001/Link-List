8898ebcf744b30d55e4a6197ce1105ddfba6de6e
TaiChi <anothai.0978452316@gmail.com>
2023-11-02 22:32:12 +0700

    create node link_list

--- Link_List.cpp
+++ Link_List.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+class Node{
+    int data;
+    Node *next;
+
+    Node(int value):data(value),next(NULL){}
+}
+
 int main(){
     cout<<"We will learn the data structure Link_List";
 }
