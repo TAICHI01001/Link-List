00d194b485ab1b34436510a6a4e174bfae12c037
TaiChi <anothai.0978452316@gmail.com>
2023-11-03 10:48:32 +0700

    create C append

--- Link_List.cpp
+++ Link_List.cpp
@@ -7,7 +7,27 @@ class Node{
     Node *next;
 
     Node(int value):data(value),next(NULL){}
-}
+};
+
+class LinkList{
+    public:
+        Node *head;
+        
+        LinkList():head(NULL){}
+
+        void append(int value){
+            Node *newNode = new Node(value);
+            if(!head){
+                head = newNode;
+            }else{
+                Node *current = head;
+                While(current->next){
+                    current = current->next;
+                }
+                current->next = newNode;
+            }
+        }
+};
 
 int main(){
     cout<<"We will learn the data structure Link_List";
